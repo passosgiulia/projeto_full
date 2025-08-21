f11aee88b93448705e1c39ea525e6b8441df18e0
GIULIA PASSOS SILVA <24153630@edu119.local>
2025-08-21 14:50:27 -0300

    Remove repositório interno frontend-tarefas

---
+++ frontend-tarefas/src/main.ts
@@ -0,0 +1,13 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+bootstrapApplication(App, {
+providers: [
+provideHttpClient(withFetch()),
+provideAnimations(),
+provideRouter(routes)
+]
+});
